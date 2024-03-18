aab5c22a1f643a81d758d6df2c981b827daf18c1
Richard Romig <rick.romig@gmail.com>
2024-03-17 20:36:36 -0400

    empty-trash.sh - added find command to list files in Trash and added explanitory text to empty_trash() function.

--- empty-trash.sh
+++ empty-trash.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2023, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 21 Nov 2023
-# Last updated : 16 Mar 2024 Version 0.1.2
+# Last updated : 17 Mar 2024 Version 0.1.4
 # Comments     : Run as a user cron job daily, weekly, or monthly
 #              : Trash directory does not exist until a file is moved to the trash.
 #              : Tested with Debian 11/12, LMDE 6, Mint 21.x, MX Linux 23.x, BunsenLabs 11.
@@ -36,8 +36,10 @@ readonly log_file="trash.log"
 
 empty_trash() {
 	if [[ $(find "$trash_dir/files" -type f | wc -l) -gt 0 ]]; then
+		printf "Trash contents:\n"
+		find "$trash_dir/files" -type f -exec basename {} \;
 		rm -rf "$trash_dir"/files/*
-		rm -rf "$trash_dir"/info/*
+		rm -f "$trash_dir"/info/*
 		printf "Trash emptied.\n"
 	else
 		printf "No trash to empty.\n"
@@ -47,7 +49,7 @@ empty_trash() {
 [[ -d "$log_dir" ]] || mkdir -p "$log_dir"
 
 {
-	printf "%s \n" "$(date '+%F %R')"
+	printf "Trash log\nDate: %s \n" "$(date '+%F %R')"
 	if [[ -d "$trash_dir" ]]; then
 		empty_trash
 	else
