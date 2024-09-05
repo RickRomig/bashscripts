55419f4743f0388314aaf6ae1946a820f26610d3
Richard Romig <rick.romig@gmail.com>
2024-09-04 23:10:38 -0400

    empty-trash.sh - checks if trash-cli is installed; uses trash-list and trash-empty to list trash contents and remove files that have been in the trash for more than 7 days.

--- empty-trash.sh
+++ empty-trash.sh
@@ -2,15 +2,15 @@
 ###############################################################################
 # Script Name  : empty-trash.sh
 # Description  : Empty the local trash directory
-# Dependencies : trash-cli tree 'sudo apt install trash-cli tree'
+# Dependencies : trash-cli 'sudo apt install trash-cli'
 # Arguments    : none
 # Author       : Copyright © 2023, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 21 Nov 2023
-# Last updated : 03 Sep 2024 Version 2.6.24247
+# Updated      : 04 Sep 2024 Version 3.1.24248
 # Comments     : Run as a user cron job.
 #              : Trash directory does not exist until a file is moved to the trash.
-#              : Tested with Debian 11/12, LMDE 6, Mint 21.x, Mint 22, MX Linux 23.1, BunsenLabs 11.
+#              : Removes files that have been in the trash folder more than a week.
 # License      : GNU General Public License, version 2.0
 ###############################################################################
 #
@@ -33,28 +33,19 @@
 readonly trash_dir="$HOME/.local/share/Trash"
 readonly log_dir="$HOME/.local/share/logs"
 readonly log_file="trash.log"
+last_week=$(date -d "$(date) - 7 days" +%F)
 
 empty_trash() {
-	if [[ $(find "$trash_dir"/info -type f | wc -l) -gt 0 ]]; then
+	if [[ $(/usr/bin/trash-list | wc -l) -gt 0 ]]; then
 		printf "\nTrash contents:\n---------------\n"
-		if [[ -x /usr/bin/tree ]]; then
-			tree -a "$trash_dir"
+		/usr/bin/trash-list
+		printf "Removing trash before %s...\n" "$last_week"
+		/usr/bin/trash-empty 7
+		if [[ $(/usr/bin/trash-list | wc -l) -gt 0 ]]; then
+			printf "Trash after %s...\n" "$last_week"
+			/usr/bin/trash-list
 		else
-			find "$trash_dir"/files -type f,d
-			find "$trash_dir"/info -type f
-		fi
-		if [[ -x usr/bin/trash-empty ]]; then
-			/usr/bin/trash-empty
-		else
-			find "$trash_dir"/files -mindepth 1 -type f,d -exec rm -rf {} +
-			rm -f "$trash_dir"/info/*
-		fi
-		printf "\nTrash emptied.\n\n"
-		if [[ -x /usr/bin/tree ]]; then
-			tree -a "$trash_dir"
-		else
-			find "$trash_dir"/files -type f,d
-			find "$trash_dir"/info -type f
+			printf "All trash removed.\n"
 		fi
 	else
 		printf "\nNo trash to empty.\n"
@@ -65,10 +56,12 @@ empty_trash() {
 
 {
 	printf "Date: %s \n" "$(date '+%F %R')"
-	if [[ -d "$trash_dir" ]]; then
+	if ! dpkg -l trash-cli > /dev/null 2>&1; then
+		printf "trash-cli package is not installed.\n"
+	elif [[ -d "$trash_dir" ]]; then
 		empty_trash
 	else
-    printf "\nTrash directory does not exist.\n"
+		printf "\nTrash directory does not exist.\nWill be created when a file is moved to the trash.\n"
 	fi
 } > "$log_dir/$log_file" 2>&1
 exit
