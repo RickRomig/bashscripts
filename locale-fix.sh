4e58aa55f398a4a5e7d36ce18acd2450887278f9
Richard Romig <rick.romig@gmail.com>
2025-01-14 19:16:00 -0500

    locale-fix.sh - implemented main()

--- locale-fix.sh
+++ locale-fix.sh
@@ -6,37 +6,39 @@
 # Arguments    : none
 # Author       : Richard Romig
 # Email        : rick.romig@gmail.com
+# Created      :
+# Updated      : 14 Jan 2025
 # Comment      :
 # License      : GNU General Public License, version 2.0
 #########################################################################
 
 # Copyright © 2023, Richard B. Romig
 # Email: rick.romig@gmail.com
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along
 # with this program. If not, see <https://www.gnu.org/licenses/>.
 
 ## Variables ##
 
-_script=$(basename "$0"); readonly _script
-readonly _version="0.2.4"
-readonly _updated="10 Jan 2024"
+script=$(basename "$0"); readonly script
+readonly version="2.4.25014"
 
 ## Functions ##
 
 show_intro() {
-  cat << ENDINTRO
-$_script changes the setting to store locales in individual locale direectories
+  local updated="14 Jan 2025"
+  cat << _INTRO_
+$script changes the setting to store locales in individual locale direectories
 instead of a single archive file.
 
 During some updates the following warning may occur:
@@ -45,8 +47,8 @@ The problem is that /usr/share/initramfs-tools/hooks/root_locale is expecting
 to see individual locale directories in /usr/lib/locale, but locale-gen is
 configured to generate an archive file by default.
 
-Version: $_version, last updated on $_updated.
-ENDINTRO
+Version: $version, last updated on $updated.
+_INTRO_
 }
 
 purge_update() {
@@ -57,9 +59,14 @@ purge_update() {
   sudo update-initramfs -u # -t (-t flag isn't listed in man page)
 }
 
+main() {
+  sudo_login 2
+  show_intro
+  purge_update
+  over_line "$script v$version"
+  exit
+}
+
 ## Execution ##
 
-sudo_login 2
-show_intro
-purge_update
-exit
+main "$@"
