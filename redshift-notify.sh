687470f3846aa412e85984c36803e47d990ec7d5
Richard Romig <rick.romig@gmail.com>
2025-01-16 10:52:27 -0500

    redshift-notify.sh - updated global variables.

--- redshift-notify.sh
+++ redshift-notify.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2024, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 28 Oct 2024
-# Last updated : 03 Nov 2024 version 1.1.24308
+# Last updated : 16 Jan 2025 version 1.2.25016
 # Comments     : Script doesn't do anything if there is no display (on a KVM or headless)
 #              : Run as cron job: */10 * * * * /home/username/.local/bin/redshift-notify.sh
 # TODO (Rick)  :
@@ -34,21 +34,22 @@
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
-DAYTIME_FILE=/tmp/daytime
-NIGHTIME_FILE=/tmp/nighttime
+DAY_FILE=/tmp/daytime
+NIGHT_FILE=/tmp/nighttime
 
 brightness=$(xrandr --verbose | grep -w Brightness -m1 | cut -d' ' -f2 )
 
-if [[ "$brightness" == "1.0" && -f "$NIGHTIME_FILE" ]]; then
-	rm "$NIGHTIME_FILE"
-elif [[ "$brightness" != "1.0" && -f "$DAYTIME_FILE" ]]; then
-	rm "$DAYTIME_FILE"
+if [[ "$brightness" == "1.0" && -f "$NIGHT_FILE" ]]; then
+	rm "$NIGHT_FILE"
+elif [[ "$brightness" != "1.0" && -f "$DAY_FILE" ]]; then
+	rm "$DAY_FILE"
 fi
 
-if [[ "$brightness" == "1.0" && ! -f "$DAYTIME_FILE" ]]; then
-	touch "$DAYTIME_FILE"
+if [[ "$brightness" == "1.0" && ! -f "$DAY_FILE" ]]; then
+	touch "$DAY_FILE"
 	notify-send -t 3500 "Redshift" "Changed to day setting"
-elif [[ "$brightness" != "1.0" && ! -f  "$NIGHTIME_FILE" ]]; then
-	touch "$NIGHTIME_FILE"
+elif [[ "$brightness" != "1.0" && ! -f  "$NIGHT_FILE" ]]; then
+	touch "$NIGHT_FILE"
 	notify-send -t 3500 "Redshift" "Changed to night setting"
 fi
+exit
